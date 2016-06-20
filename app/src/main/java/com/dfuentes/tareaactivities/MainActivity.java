27a895cd54aed87c0ab40900ebd31498ce904353
Divani Fuentes <inavid.divarolas@gmail.com>
2016-06-19 23:49:58 -0500

    First Commit

---
+++ app/src/main/java/com/dfuentes/tareaactivities/MainActivity.java
@@ -0,0 +1,51 @@
+package com.dfuentes.tareaactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button= (Button) findViewById(R.id.btnEjemplo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, confirmacion_datos.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/dfuentes/tareaactivities/confirmacion_datos.java
@@ -0,0 +1,13 @@
+package com.dfuentes.tareaactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class confirmacion_datos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmacion_datos);
+    }
+}
