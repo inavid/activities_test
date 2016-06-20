4e62f6a649489dcb7f7e35505199ae5c7c62dd35
Divani Fuentes <inavid.divarolas@gmail.com>
2016-06-20 00:25:33 -0500

    Se reciben parametros y se pintan en vista de edicion

--- app/src/main/java/com/dfuentes/tareaactivities/MainActivity.java
+++ app/src/main/java/com/dfuentes/tareaactivities/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final EditText nombre   = (EditText)findViewById(R.id.tietNombre);
+        final EditText fecha   = (EditText)findViewById(R.id.date);
+        final EditText telefono   = (EditText)findViewById(R.id.tietTelefono);
+        final EditText email   = (EditText)findViewById(R.id.tietEmail);
+        final EditText descripcion   = (EditText)findViewById(R.id.tietDescripcion);
         Button button= (Button) findViewById(R.id.btnEjemplo);
+        assert button != null;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, confirmacion_datos.class);
+                intent.putExtra("Nombre", nombre.getText().toString());
+                intent.putExtra("Fecha", fecha.getText().toString());
+                intent.putExtra("Telefono", telefono.getText().toString());
+                intent.putExtra("Email", email.getText().toString());
+                intent.putExtra("Descripcion", descripcion.getText().toString());
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/dfuentes/tareaactivities/confirmacion_datos.java
+++ app/src/main/java/com/dfuentes/tareaactivities/confirmacion_datos.java
@@ -2,6 +2,7 @@ package com.dfuentes.tareaactivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class confirmacion_datos extends AppCompatActivity {
 
@@ -9,5 +10,28 @@ public class confirmacion_datos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmacion_datos);
+
+        //Elementos vista
+        TextView tvnombre = (TextView) findViewById(R.id.tvNombre);
+        TextView tvfecha = (TextView) findViewById(R.id.tvFechab);
+        TextView tvtelefono = (TextView) findViewById(R.id.tvTelefono);
+        TextView tvemail = (TextView) findViewById(R.id.tvEmail);
+        TextView tvdescripcion = (TextView) findViewById(R.id.tvDescripcion);
+
+
+        //Obtencion de datos de vista anterior
+        Bundle parametros = getIntent().getExtras();
+        String nombre = parametros.getString("Nombre");
+        String fecha = parametros.getString("Fecha");
+        String telefono = parametros.getString("Telefono");
+        String email = parametros.getString("Email");
+        String descripcion = parametros.getString("Descripcion");
+
+        //Se ponen los datos recibidos
+        tvnombre.setText(nombre);
+        tvfecha.setText(fecha);
+        tvtelefono.setText(telefono);
+        tvemail.setText(email);
+        tvdescripcion.setText(descripcion);
     }
 }
