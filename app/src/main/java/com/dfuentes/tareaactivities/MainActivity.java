51aabe63f03f6099976f2957fc2cc792b5cc0f66
Divani Fuentes <inavid.divarolas@gmail.com>
2016-06-20 00:47:20 -0500

    Se añade funcionalidad de pruebas para boton de edicion y se termina

--- app/src/main/java/com/dfuentes/tareaactivities/MainActivity.java
+++ app/src/main/java/com/dfuentes/tareaactivities/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 
     @Override
--- app/src/main/java/com/dfuentes/tareaactivities/confirmacion_datos.java
+++ app/src/main/java/com/dfuentes/tareaactivities/confirmacion_datos.java
@@ -1,7 +1,10 @@
 package com.dfuentes.tareaactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class confirmacion_datos extends AppCompatActivity {
@@ -12,6 +15,7 @@ public class confirmacion_datos extends AppCompatActivity {
         setContentView(R.layout.activity_confirmacion_datos);
 
         //Elementos vista
+        Button edicion = (Button) findViewById(R.id.btnEdicion);
         TextView tvnombre = (TextView) findViewById(R.id.tvNombre);
         TextView tvfecha = (TextView) findViewById(R.id.tvFechab);
         TextView tvtelefono = (TextView) findViewById(R.id.tvTelefono);
@@ -21,11 +25,11 @@ public class confirmacion_datos extends AppCompatActivity {
 
         //Obtencion de datos de vista anterior
         Bundle parametros = getIntent().getExtras();
-        String nombre = parametros.getString("Nombre");
-        String fecha = parametros.getString("Fecha");
-        String telefono = parametros.getString("Telefono");
-        String email = parametros.getString("Email");
-        String descripcion = parametros.getString("Descripcion");
+        final String nombre = parametros.getString("Nombre");
+        final String fecha = parametros.getString("Fecha");
+        final String telefono = parametros.getString("Telefono");
+        final String email = parametros.getString("Email");
+        final String descripcion = parametros.getString("Descripcion");
 
         //Se ponen los datos recibidos
         tvnombre.setText(nombre);
@@ -33,5 +37,19 @@ public class confirmacion_datos extends AppCompatActivity {
         tvtelefono.setText(telefono);
         tvemail.setText(email);
         tvdescripcion.setText(descripcion);
+
+        assert edicion != null;
+        edicion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(confirmacion_datos.this, MainActivity.class);
+                intent.putExtra("Nombre", nombre);
+                intent.putExtra("Fecha", fecha);
+                intent.putExtra("Telefono", telefono);
+                intent.putExtra("Email", email);
+                intent.putExtra("Descripcion", descripcion);
+                startActivity(intent);
+            }
+        });
     }
 }
